a91e907b6fb3227eaf14b4c8fe29311530ae422c
IKBOstudent <maxpon225@gmail.com>
2022-10-21 00:27:13 +0300

    builds changed

--- src/pages/Home/BuildsBlock/BuildCard/index.jsx
+++ src/pages/Home/BuildsBlock/BuildCard/index.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React from 'react';
 
-import "./build-card.scss";
+import './build-card.scss';
+
+const SaveIcon = () => {
+    return (
+        <button type="button" className="button-icon">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 18 24"
+                fill="none"
+                role="img"
+                aria-hidden="true"
+                strokeWidth="1.5"
+                height="24">
+                <title>Saved Icon</title>
+                <path d="M16.059 23.105l-6.177-4.492a1.5 1.5 0 00-1.764 0l-6.177 4.492A.75.75 0 01.75 22.5V2.25a1.5 1.5 0 011.5-1.5h13.5a1.5 1.5 0 011.5 1.5V22.5a.75.75 0 01-1.191.605v0z"></path>
+            </svg>
+        </button>
+    );
+};
 
 const BuildCard = ({ image_url }) => {
     return (
         <div className="builds-card-box card">
-            <a href="#">
-                <img src={image_url} alt="" />
+            <div className="builds-card-box__top">
+                <a href="#">
+                    <div
+                        className="builds-card-box-img"
+                        style={{ backgroundImage: `url(${image_url})` }}></div>
 
-                <h4>PC Assembled Gaming I7-12700 – RTX 3070 8GB</h4>
-            </a>
-            <span>$2,440.00</span>
+                    <h4>PC Assembled Gaming i7-12700 – RTX 3070 8GB</h4>
+                </a>
+                <SaveIcon />
+            </div>
+            <div className="builds-card-box__bottom">
+                <span>$2,440.00</span>
+            </div>
         </div>
     );
 };
--- src/pages/Home/BuildsBlock/index.jsx
+++ src/pages/Home/BuildsBlock/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import "./builds.scss";
+import './builds.scss';
 
-import BuildCard from "./BuildCard";
+import BuildCard from './BuildCard';
 
-import img_pc1 from "@assets/img_pc1.png";
-import img_pc2 from "@assets/img_pc2.png";
-import img_pc3 from "@assets/img_pc3.png";
+import img_pc1 from '@assets/img_pc1.png';
+import img_pc2 from '@assets/img_pc2.png';
+import img_pc3 from '@assets/img_pc3.png';
 
 const BuildsBlock = () => {
-    const images = [img_pc1, img_pc2, img_pc3, img_pc2, img_pc1];
+    const images = [img_pc1, img_pc2, img_pc3, img_pc2, img_pc1, img_pc3];
     return (
         <div id="builds-block" className="builds-container">
             <h2>Builds</h2>
